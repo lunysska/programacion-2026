45c9240f03d7a8230679277f9964ec76d4a1f92d
Lunysska <lunysska@gmail.com>
2026-02-24 19:55:04 -0600

    Add files via upload

---
+++ teoria/operadorPunto/Main.py
@@ -0,0 +1,23 @@
+''' 
+Created on Feb,2019
+@author: lunysska
+
+'''
+from Cuenta import *
+
+class Main:
+	pass
+
+print ("*** 1. Imprimimos atributos desde el archivo principal")
+
+#la cantidad de argumentos que se le pasa al constructor, cambia
+cuenta1 = Cuenta( 300, "debito" )
+
+#si fueran muchos atributos, acá aparecerían muchisimas 
+#lineas
+print (cuenta1.cantidad)
+print (cuenta1.tipo)
+
+print ("\n\n*** 2. Imprimimos atributos con el método")
+cuenta1.imprimirDetalles()
+
